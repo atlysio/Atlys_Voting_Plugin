c1908a7366ae08e945e345a979f4f1e2ccceb137
hunterlong <info@socialeck.com>
2016-11-16 11:31:23 -0700

    updated

--- lib/atlys_voting/engine.rb
+++ lib/atlys_voting/engine.rb
@@ -8,11 +8,5 @@ module AtlysVoting
       end
     end
 
-    initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
-        app.config.paths["db/migrate"] += config.paths["db/migrate"].expanded
-      end
-    end
-
   end
 end
